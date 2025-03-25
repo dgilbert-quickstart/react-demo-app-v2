a33910e3bd3ac95c439d80837c481c50f7a60b8a
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-25 17:04:26 +0000

    update react demo app 2 - create-react-app, yarn, vite

--- react-demo-app-v02/src/App.js
+++ react-demo-app-v02/src/App.js
@@ -1,10 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home";
 
 function App() {
  
   return (
     <>
-      <Home/>
+      <BrowserRouter>            
+      <Routes>      
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>    
     </>
   );
 }
