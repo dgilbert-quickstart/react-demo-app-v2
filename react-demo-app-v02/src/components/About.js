3a7f46819431933911612f07c9957bc5fef7bac2
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-25 18:43:54 +0000

    update react demo app 2 - create-react-app, yarn, vite

--- react-demo-app-v02/src/components/About.js
+++ react-demo-app-v02/src/components/About.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function About()
 {
     return (
         <>
             <p>About Page</p>
+            <p></p>
+            <Link to="/">Home</Link>
         </>
     )
 }
